af33b793205e0952d79aa93e9f5e581d6d534a5a
was1499 <aniketw999@gmail.com>
2018-02-24 16:29:08 +0530

    extended the dataset

--- purpose_finder.py
+++ purpose_finder.py
@@ -3,26 +3,40 @@
 
 # DATASET
 
-# In[38]:
+# In[87]:
 
 
-data={'meet':'note','meeting':'note','wedding':'note','function':'note','alarm':'note','funeral':'note','ceremony':'note','party':'note','vacations':'note','holidays':'note','holiday':'note','break':'note','trip':'note','tour':'note','travel':'note','travelling':'note',
-     'pizza':'food','dominos':'food','pizzahut':'food','hungry':'food','hunger':'food','lunch':'food','dinner':'food','breakfast':'food','kathi':'food','khana':'food','bhojan':'food','bhook':'food','pet':'food','stomach':'food','shop':'food','nashta':'food','restaurant':'food','hotel':'food','dhaba':'food','canteen':'food','cafe':'food','motel':'food','inn':'food','cuisine':'food','multicuisine':'food','bakery':'food','chopati':'food','khana':'food','khane':'food',
-      'movie':'movie','theater':'movie','ticket':'movie','show':'movie','cinema':'movie','multiplex':'movie','picture':'movie','cine':'movie','screen':'movie','talkies':'movie','screenplay':'movie','cartoon':'movie','opera':'movie','hollywood':'movie','bollywood':'movie','film':'movie','films':'movie','jhilli':'movie','chitra':'movie','chalchitra':'movie','pardha':'movie','parda':'movie','philam':'movie',
+data={'meet':'note','meeting':'note','wedding':'note','function':'note','alarm':'note','funeral':'note','ceremony':'note','party':'note','vacations':'note','holidays':'note',
+      'holiday':'note','break':'note','trip':'note','tour':'note','travel':'note','travelling':'note','note':'note','milna':'note','mil':'note','milenge':'note','milap':'note',
+      'milunga':'note','shaadi':'note','baarat':'note','lagan':'note','tyohar':'note','maihar':'note','jalsa':'note','ghadi':'note',
+      'samaaroh':'note','jhasan':'note','chuttiyan':'note','rasham':'note','vidhi':'note','samarambh':'note','avdhi':'note','tatkal':'note','kal':'note','ghumna':'note','ghumne':'note',
+      'traveller':'note','paryatan':'note','utsav':'note','chutti':'note','weekend':'note','avkash':'note','avdhi':'note','ghumenge':'note','yatra':'note','safar':'note',
+      'dohra':'note','brahman':'note',
+      
+      'pizza':'food','dominos':'food','pizzahut':'food','hungry':'food','hunger':'food','lunch':'food','dinner':'food','breakfast':'food','kathi':'food','khana':'food','bhojan':'food',
+      'bhook':'food','pet':'food','stomach':'food','shop':'food','nashta':'food','restaurant':'food','hotel':'food','dhaba':'food','canteen':'food','cafe':'food','motel':'food','inn':'food',
+      'cuisine':'food','multicuisine':'food','bakery':'food','chopati':'food','khana':'food','khane':'food','roll':'food','bhookh':'food','bhookha':'food','food':'food',
+      
+      'movie':'movie','theater':'movie','ticket':'movie','show':'movie','cinema':'movie','multiplex':'movie','picture':'movie','cine':'movie','screen':'movie','talkies':'movie','movie':'movie',
+      'screenplay':'movie','cartoon':'movie','opera':'movie','hollywood':'movie','bollywood':'movie','film':'movie','films':'movie','jhilli':'movie','chitra':'movie','chalchitra':'movie',
+      'pardha':'movie','parda':'movie','philam':'movie','chalachitra':'movie','sinemaaghar':'movie','drome':'movie',
+      
+      'pool':'game','snooker':'game','billiards':'game','tt':'game','tennis':'game','fooseball':'game','carrom':'game','carromboard':'game','chess':'game','chessboard':'game','khelna':'game',
+      'khel':'game','game':'game'}
 
 
 # INPUT
 
-# In[41]:
+# In[85]:
 
 
-ip=""
+ip="i want to order movie"
 iplist=ip.split()
 
 
 # PURPOSE ALGO
 
-# In[42]:
+# In[86]:
 
 
 purpose='none'
